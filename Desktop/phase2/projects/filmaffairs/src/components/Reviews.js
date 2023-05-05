8aee9cc10fb9a71c5c27986861be4c025e85614b
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-05-05 08:38:48 +0300

    footer-components

--- Desktop/phase2/projects/filmaffairs/src/components/Reviews.js
+++ Desktop/phase2/projects/filmaffairs/src/components/Reviews.js
@@ -5,9 +5,9 @@ function Reviews() {
     <div className="reviews">
       <h3>Movie Reviews</h3>
       <ul>
-        <li>"This movie was amazing! I loved the storyline and the characters." - Jane D.</li>
-        <li>"I was on the edge of my seat the whole time. Highly recommend!" - John S.</li>
-        <li>"The visuals in this movie were stunning. Definitely worth a watch." - Emily T.</li>
+        <li>"This movie was amazing! I loved the storyline and the characters." - StevieG.</li>
+        <li>"I was on the edge of my seat the whole time. Highly recommend!" - Aisha S.</li>
+        <li>"The visuals in this movie were stunning. Definitely worth a watch." - Khristine G.</li>
       </ul>
     </div>
   );
